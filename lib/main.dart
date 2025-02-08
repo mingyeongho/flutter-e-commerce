c0db7230ed7dda5d04775382381c98da4c66acb6
민경호 <1736s@naver.com>
2025-02-09 03:01:50 +0900

    feat: add themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
+import 'package:e_commerce/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const App());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      themeMode: ThemeMode.system, // 플러터가 시스템의 테마를 따르도록 설정
+      theme: TAppTheme.lightTheme,
+      darkTheme: TAppTheme.darkTheme,
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
