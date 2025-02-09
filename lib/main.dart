e182eac864e106b37e35d74c7a306b65feebfb37
민경호 <1736s@naver.com>
2025-02-09 18:26:25 +0900

    chore: install packages and write util functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,6 @@
-import 'package:e_commerce/utils/theme/theme.dart';
+import 'package:e_commerce/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system, // 플러터가 시스템의 테마를 따르도록 설정
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
 }
